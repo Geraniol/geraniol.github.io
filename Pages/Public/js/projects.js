abb683f2229e51d30a8a038a945585c250700a45
防弹封雅 <42748495+Geraniol@users.noreply.github.com>
2022-10-30 02:46:57 +0800

    Add files via upload

--- Pages/Public/js/projects.js
+++ Pages/Public/js/projects.js
@@ -1,39 +1,267 @@
 var projects =
     [
         {
-            "title": "This is a title.",
-            "preview": "./Pages/Public/img/JUP.jpg",
-            "src": "./Pages/Public/img/JUP.jpg",
-            "describe": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "title": "测试卡片",
+            "preview": "./Pages/Public/img/pv/圣诞装饰.jpg",
+            "src": "./Pages/Public/img/src/圣诞装饰.png",
+            "describe": "什么技能都会。然后不用点了，原图还没上传。",
             "tag": {
                 "design": 1,
                 "modeling": 1,
-                "photography": 0,
+                "photography": 1,
                 "coding": 1,
             }
         },
         {
-            "title": "这是另一项作品",
-            "preview": "./Pages/Public/img/SAT.jpg",
-            "src": "./Pages/Public/img/SOPII.png",
-            "describe": "ifneahrniaenrfvioaenrfeajnviaunerignapinr gvjaie nrgijunaeinrvaienrivnaeirnigeanriua vjrnviueagrnvja neivngaenrgiun",
+            "title": "阿米娅",
+            "preview": "./Pages/Public/img/pv/阿米娅.jpg",
+            "src": "./Pages/Public/img/src/阿米娅.png",
+            "describe": "广州漫展上买的。二十四小时的短途旅行。手办深渊的开端。",
             "tag": {
-                "design": 1,
+                "design": 0,
                 "modeling": 0,
                 "photography": 1,
                 "coding": 0,
             }
         },
         {
-            "title": "这是又一项作品",
-            "preview": "./Pages/Public/img/CLOUD.png",
-            "src": "./Pages/Public/img/CLOUD.png",
-            "describe": "ifneahrniaenrfvioaenrfeajnviaunerignapinr gvjaie nrgijunaeinrvaienrivnaeirnigeanriua vjrnviueagrnvja neivngaenrgiun",
+            "title": "酒吧",
+            "preview": "./Pages/Public/img/pv/酒吧.jpg",
+            "src": "./Pages/Public/img/src/酒吧.png",
+            "describe": "室友的小酒吧。刚进货一瓶伏特加。",
             "tag": {
-                "design": 1,
+                "design": 0,
                 "modeling": 0,
                 "photography": 1,
-                "coding": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "星轨 × 航班",
+            "preview": "./Pages/Public/img/pv/星轨1.jpg",
+            "src": "./Pages/Public/img/src/星轨1.png",
+            "describe": "星轨。寒冷的天气里拍的。北极星在航线轨迹下方，没有进入镜头。现在很少带相机出去拍照了。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "断了一截的星轨",
+            "preview": "./Pages/Public/img/pv/星轨2.jpg",
+            "src": "./Pages/Public/img/src/星轨2.png",
+            "describe": "不知道为什么，合成的时候少了一张，于是星轨断掉了。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "山丘",
+            "preview": "./Pages/Public/img/pv/山丘.jpg",
+            "src": "./Pages/Public/img/src/山丘.png",
+            "describe": "某个艺术展添置的永久水上景观。本来是白色的，经过了反相处理。按照指示牌的提示，似乎可以爬上去？",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "猫",
+            "preview": "./Pages/Public/img/pv/猫.jpg",
+            "src": "./Pages/Public/img/src/猫.png",
+            "describe": "猫。没什么特别的。校园里随处可见。不过这是圣诞节的猫。也许有很大概率猫毛过敏。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "夜空",
+            "preview": "./Pages/Public/img/pv/夜空.jpg",
+            "src": "./Pages/Public/img/src/夜空.png",
+            "describe": "这是夜晚拍摄的照片，虽然看上去像白天一样。仔细看还能看到星星。不过新宿舍楼开工后，应该见不到这样的山丘了。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "双子",
+            "preview": "./Pages/Public/img/pv/双子.jpg",
+            "src": "./Pages/Public/img/src/双子.png",
+            "describe": "占有欲过剩可不是什么好兆头。一时冲动，荷包空空。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "兔女郎 凛",
+            "preview": "./Pages/Public/img/pv/凛1.jpg",
+            "src": "./Pages/Public/img/src/凛1.png",
+            "describe": "一个比较便宜的手办。喜欢涩涩。好看！",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "兔女郎 凛",
+            "preview": "./Pages/Public/img/pv/凛2.jpg",
+            "src": "./Pages/Public/img/src/凛2.png",
+            "describe": "这个音游增多了喜欢的风格。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "兔女郎 凛",
+            "preview": "./Pages/Public/img/pv/凛3.jpg",
+            "src": "./Pages/Public/img/src/凛3.png",
+            "describe": "为了拍出漂亮的产品图，购买了一套摄影器材。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "兔女郎 凛",
+            "preview": "./Pages/Public/img/pv/凛4.jpg",
+            "src": "./Pages/Public/img/src/凛4.png",
+            "describe": "“家有娇妻，夫复何求？”",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "兔女郎 凛",
+            "preview": "./Pages/Public/img/pv/凛5.jpg",
+            "src": "./Pages/Public/img/src/凛5.png",
+            "describe": "背景的灯光是另一张照片后期合成的。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "玻璃球",
+            "preview": "./Pages/Public/img/pv/玻璃球.jpg",
+            "src": "./Pages/Public/img/src/玻璃球.jpg",
+            "describe": "玻璃球里的世界如同水晶一般梦幻。不过现在它沾满了灰尘。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "草",
+            "preview": "./Pages/Public/img/pv/草.jpg",
+            "src": "./Pages/Public/img/src/草.jpg",
+            "describe": "纸飞机赛事期间，赛事场地旁的草。颜色很可爱。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "建筑",
+            "preview": "./Pages/Public/img/pv/建筑.jpg",
+            "src": "./Pages/Public/img/src/建筑.jpg",
+            "describe": "校园会展中心。在门口学滑板的时候拍的。学生会的首次破冰活动也在此举行。现在附近区域正在大修。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "圣诞树",
+            "preview": "./Pages/Public/img/pv/圣诞树.jpg",
+            "src": "./Pages/Public/img/src/圣诞树.jpg",
+            "describe": "书院的圣诞树。蓝色的树上布满了铃铛。同时营造出下雪的环境。在此即将迎来第三个圣诞节。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "圣诞装饰",
+            "preview": "./Pages/Public/img/pv/圣诞装饰.jpg",
+            "src": "./Pages/Public/img/src/圣诞装饰.jpg",
+            "describe": "漂亮的前辈。也许某些不良习惯是前辈教出来的。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "月亮",
+            "preview": "./Pages/Public/img/pv/月亮.jpg",
+            "src": "./Pages/Public/img/src/月亮.jpg",
+            "describe": "前苏联 3000mm 反射式镜头拍摄。现在这些仪器只有我在用。不过转接口是网购的。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "苹果电路",
+            "preview": "./Pages/Public/img/pv/微电子1.jpg",
+            "src": "./Pages/Public/img/src/微电子1.jpg",
+            "describe": "有一段时间电脑一直在渲染，积累了很多灰尘。为了除尘亲自拆卸电脑。电脑很重要，希望不要出现故障。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "苹果电路",
+            "preview": "./Pages/Public/img/pv/微电子2.jpg",
+            "src": "./Pages/Public/img/src/微电子2.jpg",
+            "describe": "终于也拥有一整套螺丝刀套装。苹果电路就像城市一样。",
+            "tag": {
+                "design": 0,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
             }
         }
     ]
