5fd0cba0b4ef10a5772f5f42f7cde1652c9adb8f
防弹封雅 <42748495+Geraniol@users.noreply.github.com>
2022-10-29 20:56:24 +0800

    Add files via upload

---
+++ Pages/Public/js/projects.js
@@ -0,0 +1,39 @@
+var projects =
+    [
+        {
+            "title": "This is a title.",
+            "preview": "./Pages/Public/img/JUP.jpg",
+            "src": "./Pages/Public/img/JUP.jpg",
+            "describe": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "tag": {
+                "design": 1,
+                "modeling": 1,
+                "photography": 0,
+                "coding": 1,
+            }
+        },
+        {
+            "title": "这是另一项作品",
+            "preview": "./Pages/Public/img/SAT.jpg",
+            "src": "./Pages/Public/img/SOPII.png",
+            "describe": "ifneahrniaenrfvioaenrfeajnviaunerignapinr gvjaie nrgijunaeinrvaienrivnaeirnigeanriua vjrnviueagrnvja neivngaenrgiun",
+            "tag": {
+                "design": 1,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 0,
+            }
+        },
+        {
+            "title": "这是又一项作品",
+            "preview": "./Pages/Public/img/CLOUD.png",
+            "src": "./Pages/Public/img/CLOUD.png",
+            "describe": "ifneahrniaenrfvioaenrfeajnviaunerignapinr gvjaie nrgijunaeinrvaienrivnaeirnigeanriua vjrnviueagrnvja neivngaenrgiun",
+            "tag": {
+                "design": 1,
+                "modeling": 0,
+                "photography": 1,
+                "coding": 1,
+            }
+        }
+    ]
